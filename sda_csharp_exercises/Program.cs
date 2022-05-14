96395d6b4260cc052ae7f890b7b16dcf8cc1fa47
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-05-14 14:25:06 +0200

    E09 solved

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -6,7 +6,35 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+
+            string goodPesel = "55030101193";
+            Console.WriteLine($"{goodPesel} is valid: {IsValidPesel(goodPesel)}");
+
+            string badPesel = "55030101194";
+            Console.WriteLine($"{badPesel} is valid: {IsValidPesel(badPesel)}");
+        }
+
+        private static bool IsValidPesel(string pesel)
+        {
+            const int PESEL_IMPORTANT_DIGITS = 10;
+            const int PESEL_CHECKSUM_INDEX = 10;
+            
+            int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
+
+            int sum = 0;
+            for (int i = 0; i < PESEL_IMPORTANT_DIGITS; i++)
+            {
+                sum += int.Parse($"{pesel[i]}") * weights[i];
+            }
+
+            int m = sum % 10;
+
+            int checksum = 10 - m;
+            checksum = (checksum == 10) ? 0 : checksum;
+
+            int encodedChecksum = int.Parse($"{pesel[PESEL_CHECKSUM_INDEX]}");
+
+            return checksum == encodedChecksum;
         }
     }
 }
