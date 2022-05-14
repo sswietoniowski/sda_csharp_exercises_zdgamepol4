e915e7f6fa01efbae0fcd630c7b74d5103d111f7
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-05-14 10:08:24 +0200

    E02 solved

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -6,7 +6,28 @@ namespace sda_csharp_exercises
     {
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            Console.Write("Podaj podstawę: ");
+            int basis = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj wykładnik: ");
+            int exponent = Convert.ToInt32(Console.ReadLine());
+            if (exponent < 0)
+            {
+                Console.WriteLine("Nie umiem policzyć wyniku dla ujemnej potęgi");
+                return;
+            }
+            Console.WriteLine($"{basis} ^ {exponent} = {Power(basis, exponent)}");
+        }
+
+        private static int Power(int basis, int exponent)
+        {
+            int result = 1;
+
+            for (int i = 1; i <= exponent; i++)
+            {
+                result *= basis;
+            }
+
+            return result;
         }
     }
 }
