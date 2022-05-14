7d54c8b59857d40720e98c27536419a9d377fb97
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-05-14 15:14:12 +0200

    E10 solved

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -4,9 +4,48 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        private static void BubbleSort(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int tmp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = tmp;
+                    }
+                }
+
+                Console.WriteLine($"Przejście: {i + 1}");
+                PrintNumbers(numbers);
+            }
+        }
+
+        private static void PrintNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            int[] numbers = { 3, 5, -1, 0, 20, 16, 1, 7 };
+
+            Console.WriteLine("Tablica przed posortowaniem: ");
+            PrintNumbers(numbers);
+            Console.WriteLine("Sortowanie tablicy:");
+
+            BubbleSort(numbers);
+
+            //Array.Sort(numbers);
+
+            Console.WriteLine("Tablica po posortowaniu: ");
+            PrintNumbers(numbers);
         }
     }
 }
