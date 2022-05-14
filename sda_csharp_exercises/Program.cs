f3dd86ef031711f5b1dc6ff538bafd0efa98fe6f
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-05-14 12:43:32 +0200

    E07 solved

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -1,12 +1,61 @@
 ﻿using System;
+using System.Text;
 
 namespace sda_csharp_exercises
 {
     class Program
     {
+        private static readonly char[] leetLetters = { '4', '3', '1', '0', '$' };
+        private static readonly char[] naturalLetters = { 'a', 'e', 'i', 'o', 's' };
+
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            string text = "Asia";
+            Console.WriteLine($"Text: {text}");
+            string leetSpeak = ToLeetSpeak(text);
+            Console.WriteLine($"Leet Speak: {leetSpeak}");
+            string decodedText = FromLeetSpeak(leetSpeak);
+            Console.WriteLine($"Decoded Text: {decodedText}");
+        }
+
+        private static string ToLeetSpeak(string text)
+        {
+            char[] characters = text.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                char lowerCharacter = Char.ToLower(characters[i]);
+
+                for (int j = 0; j < naturalLetters.Length; j++)
+                {
+                    if (lowerCharacter == naturalLetters[j])
+                    {
+                        characters[i] = leetLetters[j];
+                    }
+                }
+            }
+
+            return new String(characters);
+        }
+
+        private static string FromLeetSpeak(string leetSpeak)
+        {
+            char[] characters = leetSpeak.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++)
+            {
+                char lowerCharacter = Char.ToLower(characters[i]);
+
+                for (int j = 0; j < leetLetters.Length; j++)
+                {
+                    if (lowerCharacter == leetLetters[j])
+                    {
+                        characters[i] = naturalLetters[j];
+                    }
+                }
+            }
+
+            return new String(characters);
         }
     }
 }
