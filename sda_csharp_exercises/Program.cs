94b749ffe84ba16a7c8692999f42f9de97e9a8b5
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-05-14 12:45:43 +0200

    E08 solved

--- sda_csharp_exercises/Program.cs
+++ sda_csharp_exercises/Program.cs
@@ -4,9 +4,59 @@ namespace sda_csharp_exercises
 {
     class Program
     {
+        const int NON_UNIQUE_NUMBER = -1;
+
+        private static void PrintUniqueNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+
+                if (IsUnique(number))
+                {
+                    int fromPosition = i + 1;
+                    MarkDuplicates(numbers, number, fromPosition);
+                }
+            }
+
+            PrintOnlyUniqueNumbers(numbers);
+        }
+
+        private static bool IsUnique(int number) => number != NON_UNIQUE_NUMBER;
+
+        private static void MarkDuplicates(int[] numbers, int number, int fromPosition)
+        {
+            for (int j = fromPosition; j < numbers.Length; j++)
+            {
+                if (!IsUnique(numbers[j]))
+                {
+                    continue;
+                }
+
+                if (numbers[j] == number)
+                {
+                    numbers[j] = NON_UNIQUE_NUMBER;
+                }
+            }
+        }
+
+        private static void PrintOnlyUniqueNumbers(int[] numbers)
+        {
+            foreach (int number in numbers)
+            {
+                if (IsUnique(number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+            Console.WriteLine();
+        }
+
         static void Main(string[] args)
         {
-            // TODO: add your solution here...
+            int[] numbers = { 2, 5, 1, 2, 5 };
+
+            PrintUniqueNumbers(numbers);
         }
     }
 }
